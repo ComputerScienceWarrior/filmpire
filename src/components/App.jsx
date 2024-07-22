04d9c1e6b5f3589cf1ac6278b7dc644c346eb1f0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-22 11:11:02 -0700

    sets up useStyles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { Home, Movie, Movies, Actor, Profile, Navigation } from './index';
+import useStyles from './styles';
 
 const App = () => {
-  return (
-    <div>
-        <CssBaseline />
-        <Navigation />
-        <main>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/movies" element={<Movies />} />
-                <Route path="/movie/:id" element={<Movie />} />
-                <Route path="/actor/:id" element={<Actor />} />
-                <Route path="/profile/:id" element={<Profile />} />
-            </Routes>
-        </main>
-        <h1>Hello, World - Filmpire</h1>
-    </div> 
-  )
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Navigation />
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/movie/:id" element={<Movie />} />
+                    <Route path="/actor/:id" element={<Actor />} />
+                    <Route path="/profile/:id" element={<Profile />} />
+                </Routes>
+            </main>
+        </div> 
+    );
 };
 
 export default App;
