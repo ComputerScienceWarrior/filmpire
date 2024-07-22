cbb3993f9757c8ea2550be6b4337dffd18b60fe5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-22 07:34:55 -0700

    Creates various Routes (movies, action, etc.)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import { Home, Movie, Movies, Actor, Profile, Navigation } from './index';
 
 const App = () => {
   return (
     <div>
         <CssBaseline />
+        <Navigation />
+        <main>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/movie/:id" element={<Movie />} />
+                <Route path="/actor/:id" element={<Actor />} />
+                <Route path="/profile/:id" element={<Profile />} />
+            </Routes>
+        </main>
         <h1>Hello, World - Filmpire</h1>
     </div> 
   )
