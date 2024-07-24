e1a951156055f84e1ca96f47e5300efe2900d873
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-24 09:33:34 -0700

    adds in categories and subcategories for sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress} from '@mui/material';
+import { Divider, List, ListItemButton, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress} from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 import useStyles from './styles';
@@ -11,6 +11,19 @@ const Sidebar = ({ setMobileOpen }) => {
     const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
     const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
+    const categories = [
+        { label: 'Popular', value: 'popular'},
+        { label: 'Top Rated', value: 'top_rated'},
+        { label: 'Upcoming', value: 'upcoming'},
+    ];
+
+    const demoCategories = [
+        { label: 'Comedy', value: 'comedy'},
+        { label: 'Action', value: 'action'},
+        { label: 'Horror', value: 'horror'},
+        { label: 'Animation', value: 'animation'},
+    ];
+
     return(
         <>
             <Link to="/" className={classes.imageLink}>
@@ -20,6 +33,34 @@ const Sidebar = ({ setMobileOpen }) => {
                     alt="filmpire logo"
                 />
             </Link>
+            <Divider />
+            <List>
+                <ListSubheader>Categories</ListSubheader>
+                {categories.map(({label, value}) => (
+                    <Link key={value} className={classes.links} to="/">
+                        <ListItemButton onClick={() => {}} button>
+                            {/* <ListItemIcon>
+                                <img src={redLogo} className={classes.genreImages} height={30} />
+                            </ListItemIcon> */}
+                            <ListItemText primary={label} />
+                        </ListItemButton>
+                    </Link>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                <ListSubheader>Genres</ListSubheader>
+                {demoCategories.map(({label, value}) => (
+                    <Link key={value} className={classes.links} to="/">
+                        <ListItemButton onClick={() => {}} button>
+                            {/* <ListItemIcon>
+                                <img src={redLogo} className={classes.genreImages} height={30} />
+                            </ListItemIcon> */}
+                            <ListItemText primary={label} />
+                        </ListItemButton>
+                    </Link>
+                ))}
+            </List>
         </>
     );
 };
