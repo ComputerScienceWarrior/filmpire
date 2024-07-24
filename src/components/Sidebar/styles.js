a9012203ce1f87f50cbb947f3ef9092bd63f742c
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-24 09:33:54 -0700

    adds styling for new categories

--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -14,5 +14,12 @@ export default makeStyles((theme) => ({
             color: 'white !important',
             textDecoration: 'none',
         }
-    }
+    },
+    links: {
+        color: theme.palette.text.primary,
+        textDecoration: 'none',
+    },
+    genreImage: {
+        filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)',
+    },
 }));
