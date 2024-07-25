1ef62d11f2138479c1b316047f6164fb11410f6d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-24 21:49:41 -0700

    properly exports MovieList

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Actor } from './Actor/Actor';
 export { default as Profile } from './Profile/Profile';
 export { default as Navigation } from './Navigation/Navigation';
 export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as MovieList } from './MovieList/MovieList';
